7eb12a9b2e75c9b3af45f6fe1bb6bd94439431ea
Stephen <stephen@flowsa.com>
2011-08-07 12:47:45 +0200

    major refactoring, additional features and schema changes

--- README.md
+++ README.md
@@ -2,12 +2,17 @@ CodeIgniter YAML Forge
 ======================
 This library aims to generate database schemas from a basic YAML defined schema. I find YAML notation a lot easier to read/and write than verbose SQL scripts or $this->dbforge->add_field( ... ) and I'm hoping this abstraction will save me some time and some keystrokes. This spark depends on Dan Horrigan's CodeIgniter wrapper class for the Spyc Yaml library to parse the YAML and utilizes CodeIgniter's dbforge library to perform the database manipulation.
 
-This is just a first draft of the idea which I cooked up today... I've designed it with my conventions in mind (I use WanWizard's DataMapper ORM). More features and more customizable options will be included as and when the need arises.
+I've designed it with my conventions in mind (I use WanWizard's DataMapper ORM) which means this might not be for everyone but please do take a look. More features and more customizable options will be included as and when the need arises.
 
 Currently the library can do the following:
 
  *   Create tables
  *   Create fields
+ *   Add dummy data
+ *   Add keys
+ *   Modify columns (existing tables)
+ *   Add columns (existing tables)
+ *   Drop colums (existing tables)
  *   Automagically add relationship fields and tables (based on the DataMapper ORM conventions)
 
 Documentation
@@ -15,7 +20,7 @@ Documentation
 
 Install (this) YAML Forge spark:
 
-    php tools/spark install -v0.0.1 yaml-forge
+    php tools/spark install -v0.1.0 yaml-forge
 
 This example controller shows the basic usage:
 
@@ -23,45 +28,50 @@ This example controller shows the basic usage:
     
         public function index()
         {
-            $this->load->spark('yaml-forge/0.0.1');
+            $this->load->spark('yaml-forge/0.1.0');
         
             $this->yaml_forge->set_debug(TRUE);       // debug: verbose output, FALSE by default
             $this->yaml_forge->set_auto_id(TRUE);     // auto_id: adds an 'id' field to each table, TRUE by default
             $this->yaml_forge->set_drop_tables(TRUE); // drop_tables: drops a table before creating, FALSE by default
 
-            $this->yaml_forge->generate( APPPATH . '../sparks/yaml-forge/0.0.1/test/test_schema.yaml' );
+            $this->yaml_forge->generate( APPPATH . '../sparks/yaml-forge/0.1.0/test/test_schema.yaml' );
         }
     }
 
-This is how the YAML should be formatted:
+Write some YAML that describes your schema (look at ./tests/text_schema.yaml
+for a more detailed explanation):
 
     "users":
-      first_name: 
-        type: varchar
-        constraint: 255
-      last_name:
-        type: varchar
-        constraint: 255
+      has_one: [user_group]
+      fields: 
+        first_name: varchar|255
+        last_name: varchar|255
 
     "categories":
       has_many: [posts]
-      name:
-        type: varchar
-        constraint: 255
+      fields: 
+        name: varchar|255
 
     "posts":
-      has_many: [categories]
-      has_one: [users]
-      title:
-        type: varchar
-        constraint: 255
-      body:
-        type: text
-      date_created:
-        type: datetime
-      date_updated: "TIMESTAMP ON UPDATE CURRENT_TIMESTAMP DEFAULT CURRENT_TIMESTAMP"
+      fields:
+        has_many: [categories]
+        has_one: [users]
+        title: varchar|255
+        body: text
+        date_created: datetime
+        date_updated: "TIMESTAMP ON UPDATE CURRENT_TIMESTAMP DEFAULT CURRENT_TIMESTAMP"
+      data:
+        1:
+          title: "My first post"
+          body: "Check out this body text dudes!"
+          date_created: "2011-01-01 00:00:00"
 
+Changelog
+---------
+0.0.1 - 0.1.0
+
+ * Almost everything is different, forget the first version :)
 
 Contributing
 ------------
-Drop me a line if you use this library or have any thoughts. Contribution would also be great.
+Drop me a line if you use this library or have any thoughts about improvements. Contribution would also be great if you want to get down and dirty with my codes.
--- libraries/Yaml_forge.php
+++ libraries/Yaml_forge.php
@@ -9,7 +9,7 @@ class Yaml_forge {
 	var $auto_id = TRUE;
 	var $drop_tables = TRUE;
 
-	function __construct()
+	public function __construct()
 	{
 		$this->CI =& get_instance();
 		$this->CI->load->helper('file');
@@ -22,22 +22,22 @@ class Yaml_forge {
 		$this->dbforge =& $this->CI->dbforge;
 	}
 
-	function set_debug($bool)
+	public function set_debug($bool)
 	{
 		$this->debug = $bool;
 	}
 
-	function set_auto_id($bool)
+	public function set_auto_id($bool)
 	{
 		$this->auto_id = $bool;
 	}
 
-	function set_drop_tables($bool)
+	public function set_drop_tables($bool)
 	{
 		$this->drop_tables = $bool;
 	}
 
-	function parse( $path )
+	public function parse( $path )
 	{
 		$this->yaml_file = read_file( $path );
 		$this->yaml_arr = $this->yaml->parse_string($this->yaml_file);
@@ -45,7 +45,7 @@ class Yaml_forge {
 		return $this->yaml_arr;
 	}
 
-	function generate( $path )
+	public function generate( $path )
 	{
 		if( ! $this->parse( $path ) )
 		{
@@ -54,151 +54,262 @@ class Yaml_forge {
 
 		if($this->debug) print_r($this->yaml_arr);
 
-		foreach($this->yaml_arr as $table => $defs)
+		foreach($this->yaml_arr as $table_name => $operations)
 		{
-			if($this->drop_tables)
+			// some operations depend on order being maintained
+			$operations_order = array('fields', 'has_one', 'has_many', 'key', 'keys', 'column', 'columns', 'drop_column', 'drop_columns', 'modify_columns', 'data');
+
+			// be helpful and display and operations not permitted
+			foreach($operations as $op_name => $ops)
 			{
-				if(isset($table['fields']))
+				if( ! in_array($op_name, $operations_order) )
 				{
-					$this->dbforge->drop_table($table);
-				}
-				elseif(isset($table['data']))
-				{
-					$this->dbforge->truncate_table($table);
+					if($this->debug) echo "$table_name - operation: $op_name not defined! Check your spelling or the docs...\n";
 				}
 			}
 
-			if($this->auto_id)
+			foreach($operations_order as $op_name)
 			{
-				$this->dbforge->add_field('id');
-			}
-
-			if(isset($defs['has_many']))
-			{
-				foreach($defs['has_many'] as $rel_table)
+				if(isset($operations[$op_name]))
 				{
-					$sort_arr = array($table, $rel_table);
-					sort($sort_arr);
-					$_table = $sort_arr[0]."_".$sort_arr[1];
+					if($this->debug) echo "$table_name - operation: $op_name\n";
 
-					$this->has_many[$_table] = array(
-						singular($table) . "_id" => array(
-							'type' => 'INT',
-							'unsigned' => TRUE
-						),
-						singular($rel_table) . "_id" => array(
-							'type' => 'INT',
-							'unsigned' => TRUE
-						)
-					);
-				}
-			}
+					$meth = "add_" . $op_name;
 
-			if(isset($defs['has_one']))
-			{
-				foreach($defs['has_one'] as $rel_table)
-				{
-					$this->dbforge->add_field(
-						array(
-							singular($rel_table) . "_id" => array(
-								'type' => 'INT',
-								'unsigned' => TRUE
-							)
-						)
-					);
-				}
-			}
-
-			$fields = isset($defs['fields']) ? $defs['fields'] : array();
-
-			foreach($fields as $name => $field)
-			{
-
-				if($this->auto_id == TRUE AND $name == 'id')
-				{
-					continue;
-				}
-
-				if($this->auto_id == FALSE AND $name == 'id' AND empty($field))
-				{
-					$this->dbforge->add_field('id');
-					continue;
-				}
-
-				if( ! is_array($field) )
-				{
-					$field = preg_replace('/^(varchar|char):([0-9]{1,3})$/', '$1($2)', $field);
-
-					if($field == 'int')
+					if( ! method_exists($this, $meth))
 					{
-						$field .= " unsigned";
+						exit('Operation not defined: ' . $op_name);
 					}
 
-					$this->dbforge->add_field($name ." ". $field);
-					continue;
-				}
-				
-				if( ! isset($field['type']))
-				{
-					exit('Field must have type');
-				}
-
-				$field['type'] = strtoupper($field['type']);
-
-				$this->dbforge->add_field(
-					array($name => $field)
-				);
-
-			}
-
-			// Finally, create the table
-			$this->dbforge->create_table($table, TRUE);
-
-			// insert data
-			$data = isset($defs['data']) ? $defs['data'] : array();
-
-			// Deal with data
-			if( is_array($data) )
-			{
-				$insert = array();
-
-				foreach($data as $d_id => $d)
-				{
-					
-					$columns = $this->db->list_fields($table);
-
-					foreach($columns as $i => $k)
-					{
-						if($k == 'id')
-						{
-							unset($columns[$i]);
-							$insert['id'] = $d_id;
-						}
-					}
-
-					foreach($columns as $i => $k)
-					{
-						if(isset($d[$i]))
-						{
-							$insert[$k] = $d[$i];
-						}
-					}
-
-					$this->db->insert($table, $insert);
-				}
-			}
-
-			// Create the "has many" tables
-			if( ! empty($this->has_many))
-			{
-				$this->dbforge->add_field('id');
-				foreach($this->has_many as $table => $fields)
-				{
-					$this->dbforge->add_field($fields);
-					$this->dbforge->create_table($table, TRUE);
+					$this->$meth($table_name, $operations[$op_name]);
 				}
 			}
 		}
+
+	}
+
+	private function add_data($table, $data)
+	{
+		// Deal with data
+		if( ! is_array($data) )
+		{
+			exit($table . ': data must be defined as an associative array');
+		}
+
+		foreach($data as $d_id => $d)
+		{
+			if( ! is_array($d) )
+			{
+				echo "Oops, you data must be defined as an array...";
+				continue;
+			}
+
+
+			$columns = $this->db->list_fields($table);
+
+			$is_assoc = (is_array($d) AND ( ! count($d) OR count(array_filter(array_keys($d),'is_string')) == count($d)));
+
+			// Handle associative arrays
+			if($is_assoc)
+			{
+				$d['id'] = $d_id;
+				$this->db->insert($table, $d);
+				continue;
+			}
+
+			// Handle non-associative arrays
+
+			$insert = array();
+
+			foreach($columns as $i => $k)
+			{
+				if($k == 'id')
+				{
+					unset($columns[$i]);
+					$insert['id'] = $d_id;
+				}
+			}
+
+			$columns = array_values($columns);
+
+			foreach($columns as $i => $k)
+			{
+				if(isset($d[$i]))
+				{
+					$insert[$k] = $d[$i];
+				}
+			}
+
+			$this->db->insert($table, $insert);
+
+		}
+	}
+
+	private function add_has_one($table, $ops)
+	{
+		if( is_string($ops))
+		{
+			$ops = array($ops);
+		}
+
+		foreach($ops as $rel_table)
+		{
+			$this->dbforge->add_column(
+				$table,
+				array(
+					singular($rel_table) . "_id" => array(
+						'type' => 'INT',
+						'unsigned' => TRUE
+					)
+				)
+			);
+		}
+	}
+
+	private function add_fields($table, $ops)
+	{
+		if($this->drop_tables)
+		{
+			$this->dbforge->drop_table($table);
+		}
+
+		if($this->auto_id)
+		{
+			$this->dbforge->add_field('id');
+		}
+
+		foreach($ops as $name => $field)
+		{
+
+			if($this->auto_id == TRUE AND $name == 'id')
+			{
+				continue;
+			}
+
+			if($this->auto_id == FALSE AND $name == 'id' AND empty($field))
+			{
+				$this->dbforge->add_field('id');
+				continue;
+			}
+
+			if( ! is_array($field) )
+			{
+				$field = preg_replace('/^(varchar|char)\|([0-9]{1,3})$/', '$1($2)', $field);
+
+				if($field == 'int')
+				{
+					$field .= " unsigned";
+				}
+
+				$this->dbforge->add_field("`" . $name ."` ". $field);
+				continue;
+			}
+			
+			if( ! isset($field['type']))
+			{
+				exit('Field must have type');
+			}
+
+			$field['type'] = strtoupper($field['type']);
+
+			$this->dbforge->add_field(
+				array($name => $field)
+			);
+
+		}
+		// Finally, create the table
+		$this->dbforge->create_table($table, TRUE);
+	}
+
+	private function add_columns($table, $ops)
+	{
+		foreach($ops as $name => $field)
+		{
+			$this->dbforge->add_column($table, array(
+				$name => $field
+				)
+			);
+		}
+	}
+
+	private function add_modify_columns($table, $ops)
+	{
+		foreach($ops as $name => $field)
+		{
+			$this->dbforge->modify_column($table, array(
+				$name => $field
+				)
+			);
+		}
+	}
+
+	private function add_key($table, $ops)
+	{
+		if(is_string($ops))
+		{
+			$ops = array($ops);
+		}
+
+		$this->add_keys($table, $ops);
+	}
+
+	private function add_keys($table, $ops)
+	{
+		foreach($ops as $key_name)
+		{
+			// DBForge doesn't have the support I'd like... so SQL it is
+			$this->db->query("ALTER TABLE $table ADD key $key_name($key_name)");
+		}
+	}
+
+	private function add_drop_column($table, $ops)
+	{
+		if( is_string($ops) )
+		{
+			$ops = array($ops);
+		}
+
+		$this->add_drop_columns($table, $ops);
+	}
+
+	private function add_drop_columns($table, $ops)
+	{
+		foreach($ops as $column)
+		{
+			$this->dbforge->drop_column($table, $column);
+		}
+	}
+
+	private function add_has_many($table, $ops)
+	{
+		if( is_string($ops) )
+		{
+			$ops = array($ops);
+		}
+
+		foreach($ops as $rel_table)
+		{
+			$sort_arr = array($table, $rel_table);
+			sort($sort_arr);
+			$_table = $sort_arr[0]."_".$sort_arr[1];
+
+			$this->dbforge->add_field('id');
+			$this->dbforge->add_field(array(
+					singular($table) . "_id" => array(
+						'type' => 'INT',
+						'unsigned' => TRUE
+					),
+					singular($rel_table) . "_id" => array(
+						'type' => 'INT',
+						'unsigned' => TRUE
+					)
+				)
+			);
+
+			// Create the "has many" tables
+			$this->dbforge->create_table($table, TRUE);
+		}
+
 
 
 	}
